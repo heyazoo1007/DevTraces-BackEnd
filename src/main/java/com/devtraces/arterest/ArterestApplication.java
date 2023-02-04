05322a493b633a5b2be602055bb0107f987af7f3
heyazoo1007 <heyazoo1007@gmail.com>
2023-02-04 21:05:43 +0900

    initial commit

---
+++ src/main/java/com/devtraces/arterest/ArterestApplication.java
@@ -0,0 +1,13 @@
+package com.devtraces.arterest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArterestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArterestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devtraces/arterest/ArterestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtraces.arterest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArterestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
