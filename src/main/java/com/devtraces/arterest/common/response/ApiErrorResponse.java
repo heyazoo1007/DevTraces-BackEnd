5e4aa1f43f25aeaed781933fb83cbcee3dc8fdb5
soeunkk <luckyber1@naver.com>
2023-02-05 19:07:29 +0900

    fix: 실패 응답에서 data 필드 제거 (#1)

--- src/main/java/com/devtraces/arterest/common/response/ApiErrorResponse.java
+++ src/main/java/com/devtraces/arterest/common/response/ApiErrorResponse.java
@@ -13,15 +13,6 @@ public class ApiErrorResponse<T> {
 	private String code;
 	@JsonProperty("errorMessage")
 	private String message;
-	private T data;	// TODO: Exception에 Generic(T)를 전달할 방법을 찾지 못함
-
-	public static <T> ApiErrorResponse<T> from(ErrorCode errorCode, T data) {
-		return ApiErrorResponse.<T>builder()
-			.code(errorCode.getCode())
-			.message(errorCode.getMessage())
-			.data(data)
-			.build();
-	}
 
 	public static ApiErrorResponse<?> from(ErrorCode errorCode) {
 		return ApiErrorResponse.builder()
