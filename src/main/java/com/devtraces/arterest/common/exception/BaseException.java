5704a72cfa5f910631ef2199d38a202c69075d50
soeunkk <luckyber1@naver.com>
2023-02-05 18:32:19 +0900

    feat: 공통 예외 처리 (#1)

---
+++ src/main/java/com/devtraces/arterest/common/exception/BaseException.java
@@ -0,0 +1,20 @@
+package com.devtraces.arterest.common.exception;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public class BaseException extends RuntimeException {
+	private final ErrorCode errorCode;
+
+	// 의도적인 예외이므로 stack trace 제거 (불필요한 예외처리 비용 제거)
+	@Override
+	public synchronized Throwable fillInStackTrace() {
+		return this;
+	}
+
+	public HttpStatus getHttpStatus() {
+		return errorCode.getHttpStatus();
+	}
+}
